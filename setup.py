8fb99552562908945a7f62ddba140a1c1020148a
Fasih Ahmad Fakhri <fasih@email.com>
2020-10-19 23:35:33 +0530

    Using a src/ layout

--- setup.py
+++ setup.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import django_settings_local
 from setuptools import setup
 
-VERSION = django_settings_local.__version__
-
-setup(
-  download_url=f'https://github.com/fasih/django-settings-local/archive/{VERSION}.tar.gz',
-)
+setup()
