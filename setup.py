1c28191f6b2fb453a6ffd8a1f0d81e1316d4c296
Fasih Ahmad Fakhri <fasih@email.com>
2020-10-19 15:06:32 +0530

    Updated README

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
   name = 'django-settings-local',
   packages = ['django_settings_local'],
-  version = '0.1.3',
+  version = '0.1.4',
   description = 'Manage Django Local Settings',
   author = 'Fasih Ahmad Fakhri',
   author_email = 'fasih@email.com',
   url = 'https://github.com/fasih/django-settings-local',
-  download_url='https://github.com/fasih/django-settings-local/archive/0.1.3.tar.gz',
+  download_url='https://github.com/fasih/django-settings-local/archive/0.1.4.tar.gz',
   keywords = 'django local settings',
   entry_points={
     'console_scripts': [
